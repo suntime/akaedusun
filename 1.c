1daf71cc68af1da99f4115800a44aee90e569922
akaedu <akaedu@xt.(none)>
2012-02-06 20:50:13 +0800

    lf

---
+++ 1.c
@@ -0,0 +1,11 @@
+dlfkdfkjkj************************************************************************
+ *       Filename:  1.c
+ *    Description:  sdlkfjdsfkj
+ *        Version:  1.0
+ *        Created:  2012年02月06日 20时48分09秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *         Author:  xutao (But_Bueatiful) 775932080@qq.com
+ *   Organization:  akaedu.org
+ ************************************************************************/
+
